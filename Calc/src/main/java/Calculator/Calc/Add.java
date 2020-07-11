5745cc1be11915135bdb2300a4ae3b12fcd3d4dd
rahulbadam <rahulbadam000@gmail.com>
2020-07-11 09:19:59 +0530

    firdt commit

---
+++ Calc/src/main/java/Calculator/Calc/Add.java
@@ -0,0 +1,11 @@
+package Calculator.Calc;
+
+public class Add extends CalcMethods{
+	public Add(double x,double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+	public double add() {
+		return x+y;
+	}
+}
