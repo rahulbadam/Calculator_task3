63c12182bbf68a50be9efb46032e94ff632cbb39
rahulbadam <58477038+rahulbadam@users.noreply.github.com>
2020-07-11 09:34:20 +0530

    Update App.java

--- Calc/src/main/java/Calculator/Calc/App.java
+++ Calc/src/main/java/Calculator/Calc/App.java
@@ -1,45 +1,42 @@
 package Calculator.Calc;
 import java.util.*;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
 	public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // nextDouble() reads the next double from the keyboard
         while(true) {
-        	System.out.println();
-        	System.out.println("Enter two numbers: ");
-        double first = reader.nextDouble();
-        double second = reader.nextDouble();
-        System.out.println("Enter an operator (+, -, *, /): ");
+       	System.out.println("Enter numbers: ");
+        double a = reader.nextDouble();
+        double b = reader.nextDouble();
+        System.out.println("Select an operation \n1.Subtraction(-)\n2.Addition(+)\n3.Division(/)\n4.Multiplication(*)");
         System.out.println("Enter any other character to exit");
-        CalcMethods c = new CalcMethods(first, second);
+        CalcMethods c = new CalcMethods(a,b);
         char operator = reader.next().charAt(0);
-        double result;
+        double res;
         switch(operator)
         {
-            case '+':
-                result = c.add(first,second);
+	    case '-':
+                res = c.sub(a,b);
                 break;
-            case '-':
-                result = c.sub(first,second);
+            case '+':
+                res = c.add(a,b);
+                break;
+            
+	    case '/':
+                res = c.div(a,b);
                 break;
             case '*':
-                result = c.mul(first,second);
+                res = c.mul(a,b);
                 break;
-            case '/':
-                result = c.div(first,second);
-                break;
+            
             // operator doesn't match any case constant (+, -, *, /)
             default:
-                System.out.println("you have unfortunately Exited my calculator program :( ..We are sorry to see yoou go!.");
+                System.out.println("You have entered wrong option please try again.\n\tThankyou......");
                 reader.close();              
                 return;
         }
-        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+        System.out.printf("%.1f %c %.1f = %.1f", a, operator, b, res);
         }
     }
 }
